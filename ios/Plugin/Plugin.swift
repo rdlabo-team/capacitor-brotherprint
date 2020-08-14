d7849676c5dd6f1f2bf19a68a94aa52e30dd09cb
榊原昌彦 <sakakibara@rdlabo.jp>
2020-08-14 14:21:13 +0900

    feat(ios): 印刷まで実装

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -3,38 +3,91 @@ import Capacitor
 import BRLMPrinterKit
 
 /**
- *  こちらもインポートできないです。 `No such module 'BRLMPrinterKit'` とエラーがでます。
- *  Framework Search Paths、Header Search Paths は追加済み
- */
-// import BRLMPrinterKit
-
-/**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(BrotherPrint)
 public class BrotherPrint: CAPPlugin {
 
-    @objc func echo(_ call: CAPPluginCall) {
-//        let channel = BRLMChannel(wifiIPAddress: "IPAddress.of.your.printer")
-//
-//        let generateResult = BRLMPrinterDriverGenerator.open(channel)
-//        guard generateResult.error.code == BRLMOpenChannelErrorCode.noError,
-//            let printerDriver = generateResult.driver else {
-//                print("Error - Open Channel: %d", generateResult.error.code)
-//                return
-//        }
-//
-//        print("Success - Open Channel")
+    @objc func printImage(_ call: CAPPluginCall) {
+        let encodedImage: String = call.getString("encodedImage") ?? "";
+        if (encodedImage == "") {
+            call.error("Error - Image data is not found.");
+            return;
+        }
+
+        let newImageData = Data(base64Encoded: encodedImage, options: []);
         
-//        guard let ptp = BRPtouchPrinter(printerName: deviceName, interface: CONNECTION_TYPE.BLUETOOTH) else {
-//            print("*** Prepare Print Error ***")
-//            return
-//        }
-//
-        let value = call.getString("value") ?? ""
-        call.success([
-            "value": value
-        ])
+        let printerType: String = call.getString("printerType") ?? "";
+        if (printerType == "") {
+            call.error("Error - printerType is not found.");
+            return;
+        }
+        
+        if (printerType != "QL-820NW") {
+            // iOS非対応
+            call.error("Error - connection is not found.");
+            return;
+        }
+
+
+        // 検索からデバイス情報が得られた場合
+        let localName: String = call.getString("localName") ?? "";
+        let ipAddress: String = call.getString("ipAddress") ?? "";
+        
+        
+        DispatchQueue.main.async {
+            var channel: BRLMChannel;
+            if (localName != "") {
+                channel = BRLMChannel(bleLocalName: localName);
+            } else if (ipAddress != "") {
+                channel = BRLMChannel(wifiIPAddress: ipAddress);
+            } else {
+                // iOSは有線接続ができない
+                call.error("Error - connection is not found.");
+                return;
+            }
+            
+            let generateResult = BRLMPrinterDriverGenerator.open(channel);
+            guard generateResult.error.code == BRLMOpenChannelErrorCode.noError,
+                let printerDriver = generateResult.driver else {
+                    self.notifyListeners("onPrintFailedCommunication", data: [
+                        "value": true
+                    ]);
+                    NSLog("Error - Open Channel: \(generateResult.error.code)")
+                    return
+            }
+            
+            guard
+                let decodedByte = UIImage(data: newImageData! as Data),
+                let printSettings = BRLMQLPrintSettings(defaultPrintSettingsWith: BRLMPrinterModel.QL_820NWB)
+                else {
+                    NSLog("Error - Image file is not found.")
+                    return
+            }
+            
+            let labelNameIndex = call.getInt("labelNameIndex") ?? 16;
+            printSettings.labelSize = labelNameIndex == 16 ?
+                BRLMQLPrintSettingsLabelSize.rollW62 : BRLMQLPrintSettingsLabelSize.rollW62RB;
+            printSettings.autoCut = true
+
+            
+            let printError = printerDriver.printImage(with: decodedByte.cgImage!, settings: printSettings);
+
+
+            if printError.code != .noError {
+                self.notifyListeners("onPrintFailedCommunication", data: [
+                    "value": true
+                ]);
+                return;
+            }
+            else {
+                NSLog("Success - Print Image")
+                call.success([
+                    "value": true
+                ]);
+            }
+        }
     }
 }
+
