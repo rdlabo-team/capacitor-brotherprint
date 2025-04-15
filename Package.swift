d3cef397ab803cba81372c56cb7d58d81fd09948
榊原昌彦 <sakakibara@rdlabo.jp>
2025-04-15 17:44:14 +0900

    feat(ios): update current structure

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "RdlaboCapacitorBrotherprint",
+    name: "RdlaboCapacitorBrotherPrint",
     platforms: [.iOS(.v13)],
     products: [
         .library(
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'BrotherPrint',
+      name: 'capacitorExample',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
