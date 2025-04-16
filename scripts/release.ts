4977e3d7b53d1b08af525c953430b06cceb5d6aa
榊原昌彦 <sakakibara@rdlabo.jp>
2025-04-16 11:34:34 +0900

    feat(script): add release script

---
+++ scripts/release.ts
@@ -0,0 +1,38 @@
+import { readFileSync, writeFileSync } from 'fs';
+
+const environment = ['development', 'production'];
+const modulePath = './ios/Sources/BrotherPrintPlugin/module.modulemap' as const;
+const packagePath = './Package.swift' as const;
+
+type Env = (typeof environment)[number];
+
+const headerPath: Record<Env, string> = {
+  development:
+    '../../../demo/ios/LocalPackages/BRLMPrinterKit/Sources/BRLMPrinterKit.xcframework/ios-arm64/BRLMPrinterKit.framework/Headers/BRLMPrinterKit.h',
+  production:
+    '../../../../../../ios/LocalPackages/BRLMPrinterKit/Sources/BRLMPrinterKit.xcframework/ios-arm64/BRLMPrinterKit.framework/Headers/BRLMPrinterKit.h',
+};
+
+const packagePathMap: Record<Env, string> = {
+  development: './demo/ios/LocalPackages/BRLMPrinterKit',
+  production: '../../../ios/LocalPackages/BRLMPrinterKit',
+};
+
+if (!environment.includes((process.argv as string[])[process.argv.length - 1])) {
+  throw new Error('argv must be "development" or "production"');
+}
+
+const currentEnv = process.argv[process.argv.length - 1] as Env;
+const oppositeEnv: Env = environment.find((env) => env !== currentEnv)!;
+
+(() => {
+  const content = readFileSync(modulePath, 'utf-8');
+  const updated = content.replace(new RegExp(headerPath[oppositeEnv], 'g'), headerPath[currentEnv]);
+  writeFileSync(modulePath, updated);
+})();
+
+(() => {
+  const content = readFileSync(packagePath, 'utf-8');
+  const updated = content.replace(new RegExp(packagePathMap[oppositeEnv], 'g'), packagePathMap[currentEnv]);
+  writeFileSync(packagePath, updated);
+})();
